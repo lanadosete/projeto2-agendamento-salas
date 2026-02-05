197e50cd933d9300faf0b62584caaec32d002f0f
lanadosete <lourranilana7@gmail.com>
2026-02-05 18:09:17 -0300

    Configura conexão com PostgreSQL

---
+++ backend/database/connection.py
@@ -0,0 +1,11 @@
+import psycopg2
+import os
+
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        port=os.getenv("PGPORT", "5432"),
+        database=os.getenv("PGDATABASE", "agendamento_salas"),
+        user=os.getenv("PGUSER", "postgres"),
+        password=os.getenv("PGPASSWORD")
+    )
